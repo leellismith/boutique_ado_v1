f3b0366c47fca71627d6d7ee9d6b277b1de0c371
Lee Smith <leellismith@hotmail.co.uk>
2024-09-02 18:11:10 +0000

    Profiles - added order history and updated checkout views.

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,11 +11,11 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'order_total', 
+                       'delivery_cost', 'order_total',
                        'grand_total', 'original_bag',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
@@ -29,5 +29,3 @@ class OrderAdmin(admin.ModelAdmin):
     ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
-
-
