5d344df7261116b86ecbde698f8cc9522ffde99e
Lee Smith <leellismith@hotmail.co.uk>
2024-09-06 13:56:33 +0000

    images now showing change made to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -187,6 +187,7 @@ STATICFILES_DIRS = (os.path.join(STATIC_ROOT, 'css'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-lee-smith'
